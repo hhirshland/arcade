ec67085506a0a2f2c880c9a41fd2fdd31dc3b102
Henry Hirshland <hhirshland@gmail.com>
2022-06-19 19:47:31 -0700

    Updated submitGame to use msg.sender as address and added getLeaderboardLength
    Now submitting a game will by default assign the game to the msg.sender in the leaderboard.
    
    getLeaderboardLength returns the length to allow people to easily see how many entries there are.  Not sure this will be needed once front-end is built out, but helpful for now at least.

--- Arcade.sol
+++ Arcade.sol
@@ -1,10 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-//need to find the best version of safemath
-//import "./SafeMath.sol";
 import "./Ownable.sol";
-
 
 contract Arcade is Ownable {
     uint256 nowToDay = 86400;
@@ -12,16 +9,14 @@ contract Arcade is Ownable {
     uint256 secondPlaceCut = 25;
     uint256 thirdPlaceCut = 10;
     uint256 takeRate = 100 - firstPlaceCut - thirdPlaceCut;
-    uint256 costOfPlaying = 1e16; //1e16 = 0.01 eth
-    address payoutWallet = 0xdD870fA1b7C4700F2BD7f44238821C26f7392148;
+    uint256 costOfPlaying = 1e15; //1e15 = 0.001 eth
+    address payoutWallet = 0x3f2ff81DA0B5E957ba78A0C4Ad3272cB7d214e71; //this is my arcade payout wallet on metamask
 
-    mapping (address => uint256) public arcadeTokensAvailable;
-    mapping (uint256 => GameResult[]) public leaderboard;
-    mapping (uint256 => bool) public dayHasBeenPaidOut; 
-    mapping (uint256 => uint256) public dailyPoolValue; // day => day's pool's value
+    mapping(address => uint) public arcadeTokensAvailable;
 
-    //uint256 public contractBalance  = address(this).balance;
-    //uint256 public myBalance = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4.balance;
+    //I should probably make this a nested array, to have a leaderboard for each game
+    mapping(uint => GameResult[]) public leaderboard;
+    mapping(uint => bool) public dayHasBeenPaidOut;
 
     struct GameResult {
         string game;
@@ -30,42 +25,54 @@ contract Arcade is Ownable {
         uint256 score;
     }
 
-// example game results: "Flappybird",0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 69
-// "Flappybird",0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, 40
-// "Flappybird",0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db, 20
-    function submitGameResult(string memory _game, address _address, uint _score) public {
-        require(arcadeTokensAvailable[msg.sender] > 0, "Sorry, you need to pay to play!");
-        leaderboard[block.timestamp/nowToDay].push(GameResult(_game,_address,block.timestamp/nowToDay,_score));
+    //I should probably emit an event when game result is submitted... that will allow for front-end to wait for the event, and update data (like total pool value, leaderboard) when this happens
+    function submitGameResult(string memory _game, uint _score) public {
+        require(
+            arcadeTokensAvailable[msg.sender] > 0,
+            "Sorry, you need to pay to play!"
+        );
+        leaderboard[block.timestamp / nowToDay].push(
+            GameResult(_game, msg.sender, block.timestamp / nowToDay, _score)
+        );
         arcadeTokensAvailable[msg.sender]--;
-        dailyPoolValue[block.timestamp/nowToDay]++;
     }
 
-    function getDay() public view returns (uint256) {
-        return block.timestamp/nowToDay;
+    //Returns the current day
+    function getCurrentDay() public view returns (uint256) {
+        return block.timestamp / nowToDay;
     }
 
     //User deposits the game's cost to play, and then is able to play one game
-    function payToPlay() payable public {
+    function payToPlay() public payable {
         require(msg.value == costOfPlaying);
         arcadeTokensAvailable[msg.sender]++;
     }
 
+    //I think I can get rid of this
     function getContractBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
+    //Updates the cost of playing
     function updateCostOfPlaying(uint _newCost) public onlyOwner {
         costOfPlaying = _newCost;
     }
 
-    
-    function getMyBalance() public view returns (uint256) {
+    //Returns the message sender's arcade token balance
+    function getMyArcadeTokenBalance() public view returns (uint256) {
         return arcadeTokensAvailable[msg.sender];
     }
 
+    //Initiates the daily payout, and sets that day to paid (so it can't be done twice)
     function dailyPayOut(uint256 _day) public payable {
-        require(dayHasBeenPaidOut[_day] != true, "Sorry, this day's payout has already been distributed!");
-        require(block.timestamp/nowToDay > _day, "Sorry, this day has not yet ended!");
+        require(
+            dayHasBeenPaidOut[_day] != true,
+            "Sorry, this day's payout has already been distributed!"
+        );
+        require(
+            block.timestamp / nowToDay > _day,
+            "Sorry, this day has not yet ended!"
+        );
 
         address firstPlace;
         uint256 firstPlaceScore = 0;
@@ -75,9 +82,10 @@ contract Arcade is Ownable {
         uint256 thirdPlaceScore = 0;
         uint256 totalPool;
 
-        for (uint i = 0; i < leaderboard[_day].length ; i++) {
-            totalPool+= costOfPlaying;
-            if (leaderboard[_day][i].score > firstPlaceScore ) {
+        //Looping through the leaderboard to determine top 3 scores
+        for (uint i = 0; i < leaderboard[_day].length; i++) {
+            totalPool += costOfPlaying;
+            if (leaderboard[_day][i].score > firstPlaceScore) {
                 thirdPlace = secondPlace;
                 thirdPlaceScore = secondPlaceScore;
                 secondPlace = firstPlace;
@@ -95,19 +103,30 @@ contract Arcade is Ownable {
             }
         }
 
-        uint firstPlacePrize = totalPool * firstPlaceCut / 100;
-        uint secondPlacePrize = totalPool * secondPlaceCut / 100;
-        uint thirdPlacePrize = totalPool * thirdPlaceCut / 100;
-        uint take = totalPool * takeRate / 100;
+        uint firstPlacePrize = (totalPool * firstPlaceCut) / 100;
+        uint secondPlacePrize = (totalPool * secondPlaceCut) / 100;
+        uint thirdPlacePrize = (totalPool * thirdPlaceCut) / 100;
+        uint take = (totalPool * takeRate) / 100;
 
         payable(firstPlace).transfer(firstPlacePrize);
         payable(secondPlace).transfer(secondPlacePrize);
         payable(thirdPlace).transfer(thirdPlacePrize);
         payable(payoutWallet).transfer(take);
-        
         dayHasBeenPaidOut[_day] = true;
     }
-    
 
+    //Returns the total $ pool from the day's leaderboard
+    function getDailyPoolValue(uint _day) public view returns (uint256) {
+        return leaderboard[_day].length * costOfPlaying;
+    }
+
+    //Function to update the wallet where payout is sent.  Can only be called by contract owner.
+    function updatePayoutWallet(address _newAddress) public onlyOwner {
+        payoutWallet = _newAddress;
+    }
+
+    //Returns the length of the leaderboard
+    function getLeaderboardLength(uint256 _day) public view returns (uint256) {
+        return leaderboard[_day].length;
+    }
 }
-
